da82d37c3e394b75006ba92aaad01a4acbf1d7bc
Manish Soni <msoni@ar.iitr.ac.in>
2020-11-19 20:26:02 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+    if (req.url === '/welcome'){
+        res.write('Welcome to Dominos!');
+        res.end();
+    }
+    else if (req.url === '/contact') {
+        const contact =  {
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }
+        res.status(200).write(JSON.parse(contact));
+        res.end();
+    } else {
+        res.status(404);
+        res.end();
+    }
 }
 
 module.exports = httpServer;
+httpServer.listen(8081);

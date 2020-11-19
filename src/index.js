7aeed49d3a7890a125054662b8c5ffe80d115b93
Manish Soni <msoni@ar.iitr.ac.in>
2020-11-19 20:53:05 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
-var http = require("http");
+var express = require("express");
 
-const httpServer = http.createServer(handleServer);
+// const server = http.createServer(handleServer);
 
+const app = express();
+app.use(express.json());
 
-function handleServer(req, res) {
-    if (req.url === '/welcome'){
-        res.write('Welcome to Dominos!');
-        res.end();
+app.get('/welcome', (req, res) => {    
+    res.status(200).send('Welcome to Dominos!');
+});
+
+app.get('/contact', (req, res) => {    
+    const contact =  {
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
     }
-    else if (req.url === '/contact') {
-        const contact =  {
-            phone: '18602100000',
-            email: 'guestcaredominos@jublfood.com'
-        }
-        res.status(200).write(JSON.parse(contact));
-        res.end();
-    } else {
-        res.status(404);
-        res.end();
-    }
-}
+    res.status(200).send(contact);
+});
 
-module.exports = httpServer;
-httpServer.listen(8081);
+app.get('/:id', (req, res) => {   
+    const id = req.params.id;
+    if (id !== "welcome" && id !== "contact") {
+        res.status(404).send("404");        
+    } 
+});
+
+
+app.listen(8081, () => console.log('Started Listening'));
+
+// function handleServer(req, res) {
+//     if (req.url === '/welcome'){
+//         res.write('Welcome to Dominos!');
+//         res.end();
+//     }
+//     else if (req.url === '/contact') {
+//         const contact =  {
+//             phone: '18602100000',
+//             email: 'guestcaredominos@jublfood.com'
+//         }
+//         res.status(200).write(JSON.stringify(contact));
+//     } else {
+//         res.status(404);
+//         res.end();
+//     }
+// }
+
+// module.exports = httpServer;
+// console.log("Listening...");
+// httpServer.listen(8081);

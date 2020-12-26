eb27c10ab6259816763c2a3733d8576c2ddb67e3
Manish Soni <msoni@ar.iitr.ac.in>
2020-12-27 02:11:22 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
-var express = require("express");
+var http = require("http");
 
-// const server = http.createServer(handleServer);
+const httpServer = http.createServer(handleServer);
 
-const app = express();
-app.use(express.json());
-
-app.get('/welcome', (req, res) => {    
-    res.status(200).send('Welcome to Dominos!');
-});
-
-app.get('/contact', (req, res) => {    
-    const contact =  {
-        phone: '18602100000',
-        email: 'guestcaredominos@jublfood.com'
+function handleServer (req, res) {
+    if (req.url === '/welcome') {
+        // res.statusCode = 200;
+        // res.statusCode = 200;
+        res.write('Welcome to Dominos!');
+        res.statusCode=200;
+        res.end();
     }
-    res.status(200).send(contact);
-});
 
-app.get('/:id', (req, res) => {   
-    const id = req.params.id;
-    if (id !== "welcome" && id !== "contact") {
-        res.status(404).send("404");        
-    } 
-});
+    if (req.url === '/contact') {
+        const contact =  {
+            phone: '18602100000',
+            email: 'guestcaredominos@jublfood.com'
+        }
+        // res.status(200).send(contact);
+        res.statusCode=200;
+        res.write(JSON.stringify(contact));
+        res.end();
+    }
 
-console.log("listen");
-app.listen(8081, () => console.log('Started Listening'));
+    
+    res.write("404");  
+    res.statusCode=404;
+    res.end();
+    
+}
+
+// app.get('/welcome', (req, res) => {    
+//     res.status(200).send('Welcome to Dominos!');
+// });
+
+// app.get('/contact', (req, res) => {    
+//     const contact =  {
+//         phone: '18602100000',
+//         email: 'guestcaredominos@jublfood.com'
+//     }
+//     res.status(200).send(contact);
+// });
+
+// app.get('/:id', (req, res) => {   
+//     const id = req.params.id;
+//     if (id !== "welcome" && id !== "contact") {
+//         res.status(404).send("404");        
+//     } 
+// });
+
+// console.log("listen");
+// app.listen(8081, () => console.log('Started Listening'));
 
 
 
@@ -46,6 +70,6 @@ app.listen(8081, () => console.log('Started Listening'));
 //     }
 // }
 
-// module.exports = httpServer;
-// console.log("Listening...");
-// httpServer.listen(8081);
+module.exports = httpServer;
+console.log("Listening...");
+httpServer.listen(3000);
